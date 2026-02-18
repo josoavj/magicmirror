9ab74e4047f23ef52658333b886f0f1a40966a4d
Josoa Vonjiniaina <josoavonjiniaina13@gmail.com>
2026-02-18 20:56:55 +0300

    feat: Implémenter connectivity_service.dart pour gérer la connectivité

--- lib/core/services/connectivity_service.dart
+++ lib/core/services/connectivity_service.dart
@@ -1,1 +1,21 @@
+/// Service pour gérer la connectivité réseau
+/// À implémenter avec connectivity_plus
 
+abstract class ConnectivityService {
+  Future<bool> isConnected();
+  Stream<bool> onConnectivityChanged();
+}
+
+class ConnectivityServiceImpl implements ConnectivityService {
+  @override
+  Future<bool> isConnected() async {
+    // À implémenter
+    return true;
+  }
+
+  @override
+  Stream<bool> onConnectivityChanged() {
+    // À implémenter
+    return Stream.value(true);
+  }
+}
