3a5db8cbe6609b8ad6e43d6a54f67fcd97f3c0a9
Josoa Vonjiniaina <josoavonjiniaina13@gmail.com>
2026-02-18 20:55:42 +0300

    feat: Implémenter app_constants.dart avec constantes d'application

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,1 +1,20 @@
+class AppConstants {
+  // App Info
+  static const String appName = 'Magic Mirror';
+  static const String appVersion = '1.0.0';
 
+  // API Timeouts
+  static const Duration apiTimeout = Duration(seconds: 30);
+
+  // Camera
+  static const int cameraFps = 30;
+  static const String cameraResolution = '1080p';
+
+  // ML Model
+  static const String morphologyModel = 'morphology_detection';
+  static const int mlConfidenceThreshold = 80;
+
+  // Locations
+  static const double defaultLatitude = 48.8566;
+  static const double defaultLongitude = 2.3522;
+}
