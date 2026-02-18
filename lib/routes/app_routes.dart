51f8d26fda80d95e7561c689dac4b7d4cce552cf
Josoa Vonjiniaina <josoavonjiniaina13@gmail.com>
2026-02-18 20:52:34 +0300

    feat: Implémenter app_routes.dart avec configuration de navigation

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
 
+class AppRoutes {
+  static Map<String, WidgetBuilder> getRoutes() {
+    return {
+      '/': (context) =>
+          const Scaffold(body: Center(child: CircularProgressIndicator())),
+      '/mirror': (context) =>
+          const Scaffold(body: Center(child: Text('Mirror Screen'))),
+      '/agenda': (context) =>
+          const Scaffold(body: Center(child: Text('Agenda Screen'))),
+      '/outfit-suggestion': (context) =>
+          const Scaffold(body: Center(child: Text('Outfit Suggestion Screen'))),
+    };
+  }
+}
