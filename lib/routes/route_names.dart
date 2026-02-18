e6be253ddb98af2ca84c5a07373ca25411c7c6f4
Josoa Vonjiniaina <josoavonjiniaina13@gmail.com>
2026-02-18 20:52:47 +0300

    feat: Implémenter route_names.dart avec noms des routes

--- lib/routes/route_names.dart
+++ lib/routes/route_names.dart
@@ -1,1 +1,25 @@
+class RouteNames {
+  // Home & Navigation
+  static const String splash = '/splash';
+  static const String home = '/home';
 
+  // Mirror
+  static const String mirror = '/mirror';
+  static const String mirrorSettings = '/mirror/settings';
+
+  // Agenda
+  static const String agenda = '/agenda';
+  static const String agendaDetail = '/agenda/detail';
+
+  // Outfit Suggestion
+  static const String outfitSuggestion = '/outfit-suggestion';
+  static const String outfitDetail = '/outfit-suggestion/detail';
+
+  // Weather
+  static const String weather = '/weather';
+
+  // Settings
+  static const String settings = '/settings';
+  static const String profileSettings = '/settings/profile';
+  static const String preferencesSettings = '/settings/preferences';
+}
