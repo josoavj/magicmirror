5c10e1bbe7b99eb75094f80adf6bed7854d15e1f
Josoa Vonjiniaina <josoavonjiniaina13@gmail.com>
2026-02-18 20:53:55 +0300

    feat: Implémenter di_setup.dart avec injection de dépendances

--- lib/config/di_setup.dart
+++ lib/config/di_setup.dart
@@ -1,1 +1,36 @@
+/// Injection de Dépendances (Dependency Injection)
+///
+/// À implémenter avec GetIt ou un autre package DI
+///
+/// Exemple avec GetIt:
+/// final getIt = GetIt.instance;
+///
+/// getIt.registerSingleton<ConnectivityService>(ConnectivityServiceImpl());
+/// getIt.registerSingleton<StorageService>(StorageServiceImpl());
 
+class DISetup {
+  static Future<void> setupDependencies() async {
+    // TODO: Implémenter l'injection de dépendances
+    // 1. Services de base
+    // 2. Data sources
+    // 3. Repositories
+    // 4. Use cases
+    // 5. Providers (pour Riverpod ou Provider)
+  }
+
+  static void _setupCoreServices() {
+    // TODO: Setup des services de base
+  }
+
+  static void _setupDataSources() {
+    // TODO: Setup des data sources
+  }
+
+  static void _setupRepositories() {
+    // TODO: Setup des repositories
+  }
+
+  static void _setupProviders() {
+    // TODO: Setup des providers d'état
+  }
+}
