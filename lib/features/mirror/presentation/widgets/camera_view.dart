1419490cf06b8ab80703cedc726630d5f0963a16
Josoa Vonjiniaina <josoavonjiniaina13@gmail.com>
2026-02-19 22:47:27 +0300

    feat: Implémenter camera_view.dart widget caméra

--- lib/features/mirror/presentation/widgets/camera_view.dart
+++ lib/features/mirror/presentation/widgets/camera_view.dart
@@ -1,1 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
 
+class CameraView extends StatelessWidget {
+  final CameraController controller;
+  final VoidCallback? onCapturePressed;
+
+  const CameraView({
+    super.key,
+    required this.controller,
+    this.onCapturePressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    return Stack(
+      children: [
+        // Camera Preview
+        CameraPreview(controller),
+        
+        // Capture Button
+        Positioned(
+          bottom: 20,
+          left: 0,
+          right: 0,
+          child: Center(
+            child: GestureDetector(
+              onTap: onCapturePressed,
+              child: Container(
+                width: 70,
+                height: 70,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: Colors.white,
+                    width: 4,
+                  ),
+                ),
+                child: Container(
+                  margin: const EdgeInsets.all(4),
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
