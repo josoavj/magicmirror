37674249b1b4cea295e85e493f96dd146884d1f4
Josoa Vonjiniaina <josoavonjiniaina13@gmail.com>
2026-03-02 18:14:37 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,128 +1,114 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
-import 'config/di_setup.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // Setup Dependency Injection
-  await DISetup.setupDependencies();
-
-  runApp(const MyApp());
-}
+import 'core/constants/colors.dart';
+import 'core/theme/app_theme.dart';
+import 'core/constants/app_constants.dart';
+import 'core/utils/responsive_helper.dart';
+import 'config/app_config.dart';
+import 'routes/app_routes.dart';
+import 'features/mirror/presentation/screens/mirror_screen.dart';
+import 'features/agenda/presentation/screens/agenda_screen.dart';
+import 'features/outfit_suggestion/presentation/screens/outfit_suggestion_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: AppConstants.appName,
+      theme: AppTheme.lightTheme(),
+      darkTheme: AppTheme.darkTheme(),
+      themeMode: AppConfig.enableDarkMode ? ThemeMode.dark : ThemeMode.light,
+      locale: AppConfig.defaultLocale,
+      debugShowCheckedModeBanner: AppConfig.isDevelopment,
+      routes: {
+        '/': (context) => const _HomeScreen(),
+        '/mirror': (context) => const MirrorScreen(),
+        '/agenda': (context) => const AgendaScreen(),
+        '/outfit-suggestion': (context) => const OutfitSuggestionScreen(),
+      },
+      home: const _HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _HomeScreen extends StatelessWidget {
+  const _HomeScreen();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    bool isMirror = ResponsiveHelper.isMirror(context);
+
+    // Si c'est un miroir, on saute l'accueil et on va directement au miroir plein écran
+    if (isMirror) {
+      return const MirrorScreen();
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      appBar: AppBar(title: const Text('Magic Mirror'), elevation: 0),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You have pushed the button this many times:'),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Bienvenue sur Magic Mirror',
+              style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                fontSize: ResponsiveHelper.resp(
+                  context,
+                  mobile: 24,
+                  mirror: 48,
+                ),
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 32),
+            _buildMenuButton(
+              context,
+              'Miroir',
+              Icons.camera,
+              AppColors.primary,
+              () => Navigator.pushNamed(context, '/mirror'),
+            ),
+            const SizedBox(height: 16),
+            _buildMenuButton(
+              context,
+              'Agenda',
+              Icons.calendar_today,
+              AppColors.secondary,
+              () => Navigator.pushNamed(context, '/agenda'),
+            ),
+            const SizedBox(height: 16),
+            _buildMenuButton(
+              context,
+              'Suggestions de Tenue',
+              Icons.checkroom,
+              AppColors.accent,
+              () => Navigator.pushNamed(context, '/outfit-suggestion'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    );
+  }
+
+  Widget _buildMenuButton(
+    BuildContext context,
+    String label,
+    IconData icon,
+    Color color,
+    VoidCallback onPressed,
+  ) {
+    return SizedBox(
+      width: 200,
+      height: 60,
+      child: ElevatedButton.icon(
+        onPressed: onPressed,
+        icon: Icon(icon),
+        label: Text(label),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: color,
+          foregroundColor: Colors.white,
+        ),
       ),
     );
   }
