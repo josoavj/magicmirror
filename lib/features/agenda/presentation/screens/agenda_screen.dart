86dfaaf98afb05b31db5a82dfb8224bb78b1f1cc
Josoa Vonjiniaina <josoavonjiniaina13@gmail.com>
2026-03-02 19:08:30 +0300

    Update agenda_screen.dart

--- lib/features/agenda/presentation/screens/agenda_screen.dart
+++ lib/features/agenda/presentation/screens/agenda_screen.dart
@@ -1,147 +1,261 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:intl/intl.dart';
+import 'dart:ui';
 import '../providers/agenda_provider.dart';
-import '../../data/models/event_model.dart';
-import '../../../../core/utils/responsive_helper.dart';
-import '../../../../core/constants/colors.dart';
 
 class AgendaScreen extends ConsumerWidget {
   const AgendaScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final eventsAsync = ref.watch(agendaEventsProvider);
-    final isMirror = ResponsiveHelper.isMirror(context);
+    final agendaState = ref.watch(agendaEventsProvider);
 
     return Scaffold(
-      backgroundColor: isMirror ? Colors.black : null,
-      appBar: isMirror
-          ? null
-          : AppBar(title: const Text('Mon Agenda'), elevation: 0),
-      body: eventsAsync.when(
-        data: (events) => _buildContent(context, events, isMirror),
-        loading: () => const Center(child: CircularProgressIndicator()),
-        error: (err, stack) => Center(child: Text('Erreur: $err')),
-      ),
-    );
-  }
-
-  Widget _buildContent(
-    BuildContext context,
-    List<AgendaEvent> events,
-    bool isMirror,
-  ) {
-    if (isMirror) {
-      return _buildMirrorView(context, events);
-    }
-    return _buildMobileView(context, events);
-  }
-
-  Widget _buildMobileView(BuildContext context, List<AgendaEvent> events) {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16),
-      itemCount: events.length,
-      itemBuilder: (context, index) => _EventTile(event: events[index]),
-    );
-  }
-
-  Widget _buildMirrorView(BuildContext context, List<AgendaEvent> events) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 80),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'Agenda du Jour',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: ResponsiveHelper.resp(context, mobile: 24, mirror: 54),
-              fontWeight: FontWeight.bold,
-            ),
+      extendBodyBehindAppBar: true,
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [Color(0xFF0F172A), Color(0xFF1E293B)],
           ),
-          const SizedBox(height: 10),
-          Text(
-            DateFormat('EEEE d MMMM yyyy', 'fr_FR').format(DateTime.now()),
-            style: const TextStyle(color: Colors.white70, fontSize: 24),
-          ),
-          const SizedBox(height: 60),
-          Expanded(
-            child: ListView.separated(
-              itemCount: events.length,
-              separatorBuilder: (_, __) => const SizedBox(height: 30),
-              itemBuilder: (context, index) =>
-                  _MirrorEventItem(event: events[index]),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class _EventTile extends StatelessWidget {
-  final AgendaEvent event;
-  const _EventTile({required this.event});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.only(bottom: 12),
-      child: ListTile(
-        title: Text(event.title),
-        subtitle: Text(
-          '${DateFormat.Hm().format(event.startTime)} - ${DateFormat.Hm().format(event.endTime)}',
         ),
-        trailing: event.location != null
-            ? const Icon(Icons.location_on, size: 16)
-            : null,
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Planning',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 34,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: -1,
+                          ),
+                        ),
+                        Text(
+                          'Aujourd\'hui',
+                          style: TextStyle(
+                            color: Colors.white.withValues(alpha: 0.5),
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                    _GlassIconButton(
+                      icon: Icons.sync,
+                      onPressed: () => ref.read(agendaEventsProvider.notifier).syncWithGoogle(),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: agendaState.when(
+                  loading: () => const Center(child: CircularProgressIndicator(color: Colors.white)),
+                  error: (err, stack) => Center(
+                    child: Text('Erreur: $err', style: const TextStyle(color: Colors.white)),
+                  ),
+                  data: (events) => ListView.builder(
+                    padding: const EdgeInsets.symmetric(horizontal: 24),
+                    itemCount: events.length,
+                    itemBuilder: (context, index) {
+                      final event = events[index];
+                      final now = DateTime.now();
+                      final isNow = now.isAfter(event.startTime) && now.isBefore(event.endTime);
+                      
+                      return _AgendaGlassTile(
+                        time: '${event.startTime.hour.toString().padLeft(2, '0')}:${event.startTime.minute.toString().padLeft(2, '0')}',
+                        title: event.title,
+                        type: event.eventType,
+                        isNow: isNow,
+                      );
+                    },
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: _GlassButton(
+                  label: 'Retour au Miroir',
+                  onPressed: () => Navigator.pop(context),
+                  icon: Icons.arrow_back_ios_new,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
 
-class _MirrorEventItem extends StatelessWidget {
-  final AgendaEvent event;
-  const _MirrorEventItem({required this.event});
+class _AgendaGlassTile extends StatelessWidget {
+  final String time;
+  final String title;
+  final String type;
+  final bool isNow;
+
+  const _AgendaGlassTile({
+    required this.time,
+    required this.title,
+    required this.type,
+    this.isNow = false,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          width: 8,
-          height: 80,
+    return Container(
+      margin: const EdgeInsets.only(bottom: 16),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(24),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: isNow 
+                ? Colors.blue.withValues(alpha: 0.15) 
+                : Colors.white.withValues(alpha: 0.08),
+              borderRadius: BorderRadius.circular(24),
+              border: Border.all(
+                color: isNow 
+                  ? Colors.blue.withValues(alpha: 0.3) 
+                  : Colors.white.withValues(alpha: 0.1),
+                width: 1,
+              ),
+            ),
+            child: Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      time,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      type,
+                      style: TextStyle(
+                        color: Colors.white.withValues(alpha: 0.5),
+                        fontSize: 12,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(width: 24),
+                Expanded(
+                  child: Text(
+                    title,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                if (isNow)
+                  Container(
+                    width: 8,
+                    height: 8,
+                    decoration: const BoxDecoration(
+                      color: Colors.blue,
+                      shape: BoxShape.circle,
+                      boxShadow: [
+                        BoxShadow(color: Colors.blue, blurRadius: 8, spreadRadius: 2),
+                      ],
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _GlassIconButton extends StatelessWidget {
+  final IconData icon;
+  final VoidCallback onPressed;
+
+  const _GlassIconButton({required this.icon, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+        child: Container(
           decoration: BoxDecoration(
-            color: AppColors.secondary,
-            borderRadius: BorderRadius.circular(4),
+            color: Colors.white.withValues(alpha: 0.1),
+            borderRadius: BorderRadius.circular(16),
+            border: Border.all(color: Colors.white.withValues(alpha: 0.1)),
+          ),
+          child: IconButton(
+            icon: Icon(icon, color: Colors.white, size: 24),
+            onPressed: onPressed,
           ),
         ),
-        const SizedBox(width: 25),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              DateFormat.Hm().format(event.startTime),
-              style: const TextStyle(color: Colors.white60, fontSize: 22),
+      ),
+    );
+  }
+}
+
+class _GlassButton extends StatelessWidget {
+  final String label;
+  final VoidCallback onPressed;
+  final IconData icon;
+
+  const _GlassButton({required this.label, required this.onPressed, required this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(24),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
+          child: Container(
+            height: 64,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white.withValues(alpha: 0.08),
+              borderRadius: BorderRadius.circular(24),
+              border: Border.all(color: Colors.white.withValues(alpha: 0.1)),
             ),
-            Text(
-              event.title,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 32,
-                fontWeight: FontWeight.w500,
-              ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(icon, color: Colors.white, size: 18),
+                const SizedBox(width: 12),
+                Text(
+                  label,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
             ),
-            if (event.location != null)
-              Text(
-                event.location!,
-                style: const TextStyle(color: Colors.white54, fontSize: 20),
-              ),
-          ],
+          ),
         ),
-      ],
+      ),
     );
   }
 }
