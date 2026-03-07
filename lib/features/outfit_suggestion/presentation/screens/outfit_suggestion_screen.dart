be6460fd2e570c4f20096b96e56235e68737a9fb
Josoa Vonjiniaina <josoavonjiniaina13@gmail.com>
2026-03-07 14:15:51 +0300

    refactor: Improve outfit suggestion screen
    - Better UI organization and styling
    - Enhanced error handling
    - Improved user experience flow
    - Consistent widget composition

--- lib/features/outfit_suggestion/presentation/screens/outfit_suggestion_screen.dart
+++ lib/features/outfit_suggestion/presentation/screens/outfit_suggestion_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'dart:ui';
 import '../../../../presentation/widgets/glass_container.dart';
 
 class OutfitSuggestionScreen extends ConsumerWidget {
