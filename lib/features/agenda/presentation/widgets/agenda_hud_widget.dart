b214ddc7eb2f5eb905bce4aa169f3fc83e7284cd
Josoa Vonjiniaina <josoavonjiniaina13@gmail.com>
2026-03-24 10:15:00 +0300

    Update agenda_hud_widget.dart

--- lib/features/agenda/presentation/widgets/agenda_hud_widget.dart
+++ lib/features/agenda/presentation/widgets/agenda_hud_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
+import 'package:magicmirror/core/utils/responsive_helper.dart';
 import 'package:magicmirror/features/agenda/data/models/event_model.dart';
 import '../providers/agenda_provider.dart';
 
@@ -16,31 +17,51 @@ class AgendaHUDWidget extends ConsumerWidget {
       error: (err, stack) => const SizedBox.shrink(),
       data: (events) {
         if (events.isEmpty) return const SizedBox.shrink();
-        
+
         return Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisSize: MainAxisSize.min,
           children: [
-            const Padding(
-              padding: EdgeInsets.only(left: 8.0, bottom: 8.0),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0, bottom: 8.0),
               child: Text(
                 'Prochains événements',
                 style: TextStyle(
                   color: Colors.white70,
-                  fontSize: 14,
+                  fontSize: ResponsiveHelper.resp(
+                    context,
+                    mobile: 12,
+                    tablet: 14,
+                  ),
                   fontWeight: FontWeight.w600,
                   letterSpacing: 0.5,
                 ),
               ),
             ),
-            ...events.take(3).map((event) => _buildEventItem(event)),
+            ...events.take(3).map((event) => _EventItemWidget(event: event)),
           ],
         );
       },
     );
   }
 
-  Widget _buildEventItem(AgendaEvent event) {
+  static Color _getTypeColor(String type) {
+    return switch (type) {
+      'Travail' => Colors.blueAccent,
+      'Routine' => Colors.greenAccent,
+      'Google' => Colors.redAccent,
+      _ => Colors.white30,
+    };
+  }
+}
+
+class _EventItemWidget extends StatelessWidget {
+  final AgendaEvent event;
+
+  const _EventItemWidget({required this.event});
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.only(bottom: 8),
       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
@@ -55,7 +76,7 @@ class AgendaHUDWidget extends ConsumerWidget {
             width: 4,
             height: 32,
             decoration: BoxDecoration(
-              color: _getTypeColor(event.eventType),
+              color: _AgendaHUDWidgetState_getTypeColor(event.eventType),
               borderRadius: BorderRadius.circular(2),
             ),
           ),
@@ -66,9 +87,13 @@ class AgendaHUDWidget extends ConsumerWidget {
               children: [
                 Text(
                   event.title,
-                  style: const TextStyle(
+                  style: TextStyle(
                     color: Colors.white,
-                    fontSize: 15,
+                    fontSize: ResponsiveHelper.resp(
+                      context,
+                      mobile: 13,
+                      tablet: 15,
+                    ),
                     fontWeight: FontWeight.w500,
                   ),
                 ),
@@ -77,7 +102,11 @@ class AgendaHUDWidget extends ConsumerWidget {
                   DateFormat.Hm().format(event.startTime),
                   style: TextStyle(
                     color: Colors.white.withValues(alpha: 0.5),
-                    fontSize: 12,
+                    fontSize: ResponsiveHelper.resp(
+                      context,
+                      mobile: 11,
+                      tablet: 12,
+                    ),
                   ),
                 ),
               ],
@@ -87,13 +116,13 @@ class AgendaHUDWidget extends ConsumerWidget {
       ),
     );
   }
+}
 
-  Color _getTypeColor(String type) {
-    return switch (type) {
-      'Travail' => Colors.blueAccent,
-      'Routine' => Colors.greenAccent,
-      'Google' => Colors.redAccent,
-      _ => Colors.white30,
-    };
-  }
+Color _AgendaHUDWidgetState_getTypeColor(String type) {
+  return switch (type) {
+    'Travail' => Colors.blueAccent,
+    'Routine' => Colors.greenAccent,
+    'Google' => Colors.redAccent,
+    _ => Colors.white30,
+  };
 }
